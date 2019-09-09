606c5c58d660df454ca98ac11a4580c261cd3337
ehsrv <earlcoder@gmail.com>
2019-09-09 10:51:51 -0700

    Fix import path

--- admin/server.go
+++ admin/server.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql" // import driver for gorm
 	"github.com/sirupsen/logrus"
 	"github.com/videocoin/adminpanel/config"
+	"github.com/videocoin/cloud-admin/admin"
 	profiles_v1 "github.com/videocoin/cloud-api/profiles/v1"
 	transcoder_v1 "github.com/videocoin/cloud-api/transcoder/v1"
 	workorder_v1 "github.com/videocoin/cloud-api/workorder/v1"
@@ -32,7 +33,7 @@ func Start() {
 		logrus.Fatal(err)
 	}
 
-	//admin.AdminUserMigration.Migrate(db)
+	admin.AdminUserMigration.Migrate(db)
 
 	{
 		r := gin.New()
