4d05430975d6b1a5a6123102f54fe3da72ee34ef
cyc60 <avsysoev60@gmail.com>
2019-10-26 13:50:14 +0300

    Update

--- src/project/settings/staging.py
+++ src/project/settings/staging.py
@@ -3,23 +3,6 @@ from project.settings.base import *  # nopep8
 DEBUG = False
 USE_HTTPS = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-#
-# if SENTRY_DSN:
-#     INSTALLED_APPS += (
-#         'raven.contrib.django.raven_compat',
-#     )
-#
-#     RAVEN_CONFIG = {
-#         'dsn': SENTRY_DSN,
-#     }
-#
-#     LOGGING['handlers']['sentry'] = {
-#             'level': 'ERROR',
-#             'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
-#     }
-#
-#     for key, logger, in LOGGING['loggers'].items():
-#         logger['handlers'].append('sentry')
 
 if 'migrate' in sys.argv:
     # only change this for loaddata command.
