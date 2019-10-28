1a26c5fda331a69b4943ad0fb406920ce8357be8
cyc60 <avsysoev60@gmail.com>
2019-10-28 23:48:33 +0300

    Add current task to miners

--- src/apps/miners/admin.py
+++ src/apps/miners/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 
 from .models import Miner
 
 
 @admin.register(Miner)
 class MinerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'status')
+    list_display = ('id', 'status', 'current_task_link')
+    readonly_fields = ('id', 'current_task_link')
     list_filter = ('status', )
+
+    def current_task_link(self, obj):
+        if obj.current_task_id:
+            return format_html('<a href="{}">{}</a>', reverse('admin:streams_task_change', args=[obj.current_task_id]),
+                               obj.current_task_id)
+        return ''
 
     def has_delete_permission(self, request, obj=None):
         return False
--- src/apps/miners/models.py
+++ src/apps/miners/models.py
@@ -10,6 +10,7 @@ class Miner(models.Model):
     id = models.CharField(primary_key=True, max_length=255, editable=False)
     user_id = models.CharField(max_length=255, null=True, blank=True)
     status = models.CharField(max_length=100, null=True, blank=True)
+    current_task_id = models.CharField(max_length=255, null=True, blank=True)
 
     class Meta:
         db_table = 'miners'
